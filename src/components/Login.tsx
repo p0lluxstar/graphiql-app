941a6587bda000ef241c9bdff194957e788594e5
p0lluxstar <web-registra@yandex.ru>
2024-08-26 00:39:28 +0300

    init: yup; feat: add inputs for form login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,55 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
 import styles from '../styles/components/login.module.css';
+import { yupResolver } from '@hookform/resolvers/yup';
+import yupSchema from '@/utils/yupSchema';
+
+interface FormValues {
+  name: string;
+  password: string;
+}
 
 export default function Login(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+    resolver: yupResolver(yupSchema),
+  });
+
+  const onSubmit = (): void => {};
+
   return (
     <div className={styles.login}>
       <h1>Login</h1>
-      <form className={styles.form}>
-        <input id="name" placeholder="Name" type="text" />
-        <input placeholder="Password" type="password" />
-        <button>Login</button>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.name}>
+          <input
+            {...register('name')}
+            id="name"
+            placeholder="Name"
+            type="text"
+          />
+          {errors.name && <p className={styles.error}>{errors.name.message}</p>}
+        </div>
+        <div className={styles.password}>
+          <input
+            {...register('password')}
+            placeholder="Password"
+            type="password"
+          />
+          {errors.password && (
+            <p className={styles.error}>{errors.password.message}</p>
+          )}
+        </div>
+        <div className={styles.btn}>
+          <button disabled={!isValid} type="submit">
+            Login
+          </button>
+        </div>
       </form>
     </div>
   );
