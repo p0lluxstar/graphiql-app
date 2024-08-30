1cc68b464790d5d8fd71e10c27807cd03003b996
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-08-30 22:31:27 +0300

    feat: Update main component with i18n support and user authentication forms

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,60 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useTranslations } from 'next-intl';
+import styles from '../styles/components/main.module.css';
 
 export default function Main(): JSX.Element {
   const t = useTranslations();
+  const pathname = usePathname();
+  const currentLocale = pathname.split('/')[1];
 
   return (
-    <>
-      <h1>{t('main')}</h1>
-    </>
+    <main className={styles.main}>
+      <h2 className={styles.heading}>{t('welcome')}</h2>
+      <p className={styles.promo}>{t('promo')}</p>
+
+      <div className={styles.userAuth}>
+        <h3>{t('singInToContinue')}</h3>
+        <ul>
+          <li>
+            <Link
+              href={`/${currentLocale}/login`}
+              className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/login` ? styles.active : ''}`}
+            >
+              {t('login')}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={`/${currentLocale}/registration`}
+              className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/registration` ? styles.active : ''}`}
+            >
+              {t('registration')}
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.description}>
+        <div className={styles.boxDesc}>
+          <h3>{t('restClient')}</h3>
+          <ul className={styles.listDesc}>
+            <li>{t('restClientPlus1')}</li>
+            <li>{t('restClientPlus2')}</li>
+            <li>{t('restClientPlus3')}</li>
+          </ul>
+        </div>
+
+        <div className={styles.boxDesc}>
+          <h3>{t('graphiqlClient')}</h3>
+          <ul className={styles.listDesc}>
+            <li>{t('graphiqlClientPlus1')}</li>
+            <li>{t('graphiqlClientPlus2')}</li>
+            <li>{t('graphiqlClientPlus3')}</li>
+          </ul>
+        </div>
+      </div>
+    </main>
   );
 }
