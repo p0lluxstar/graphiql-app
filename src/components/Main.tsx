855ae5d79a7c3d63076bcb2e008ce7957dc5a970
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-08-31 11:27:21 +0300

    feat: Update main page and add greeting message

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,38 +3,52 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import styles from '../styles/pages/main.module.css';
+import useAuth from '@/hooks/useAuth';
+import Loader from './Loader';
 
 export default function Main(): JSX.Element {
   const t = useTranslations();
   const pathname = usePathname();
   const currentLocale = pathname.split('/')[1];
+  const { user, loading } = useAuth();
+
+  if (loading) return <Loader />;
 
   return (
     <>
       <h2 className={styles.heading}>{t('welcome')}</h2>
       <p className={styles.promo}>{t('promo')}</p>
 
-      <div className={styles.userAuth}>
-        <h3>{t('singInToContinue')}</h3>
-        <ul>
-          <li>
-            <Link
-              href={`/${currentLocale}/login`}
-              className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/login` ? styles.active : ''}`}
-            >
-              {t('login')}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={`/${currentLocale}/registration`}
-              className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/registration` ? styles.active : ''}`}
-            >
-              {t('registration')}
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {user ? (
+        <div className={styles.userAuth}>
+          <h3 className={styles.headingGreeting}>
+            {t('greeting')}, {user.email?.split('@')[0]}
+          </h3>
+          <p className={styles.subheading}>{t('continueToWork')}</p>
+        </div>
+      ) : (
+        <div className={styles.userAuth}>
+          <h3>{t('singInToContinue')}</h3>
+          <ul>
+            <li>
+              <Link
+                href={`/${currentLocale}/login`}
+                className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/login` ? styles.active : ''}`}
+              >
+                {t('login')}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={`/${currentLocale}/registration`}
+                className={`${styles.userAuthItem} ${pathname === `/${currentLocale}/registration` ? styles.active : ''}`}
+              >
+                {t('registration')}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
 
       <div className={styles.description}>
         <div className={styles.boxDesc}>
